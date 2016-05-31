e96067d20809bd0b08dcffe077f078e3c8bd4eba
Prithvi Jutur <prithvij@yahoo-inc.com>
2016-05-31 10:02:23 -0700

    test

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(process.env.OPENSHIFT_NODEJS_PORT || 80, function () {
+app.listen(process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", process.env.OPENSHIFT_NODEJS_PORT || 8080, function () {
   console.log('Example app listening on port 80!');
 });
