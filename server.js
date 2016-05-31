f2d7955da380d46b2507c8d8f24c06e1a8d37ee9
Prithvi Jutur <prithvij@yahoo-inc.com>
2016-05-31 11:55:50 -0700

    blah

--- server.js
+++ server.js
@@ -17,22 +17,6 @@ app.get('/webhook', function (req, res) {
   res.send('Error, wrong validation token');
 })
 
-app.post('/webhook', function (req, res) {
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      console.log(text);
-      sendTextMessage(sender, text);
-    }
-  }
-  res.status(200);
-});
-
-var token = "EAAV2q4QDamwBABUMgjZBOyGRt8d8x9qZAO27CFEfbnZAOYZADrMe2B2oHIYQvHPVZCBTZB4sJ98LqF58ZBPmFOrTzp65XYsS0ZAeCw26LukdUCD6Q9r4w8M45HKi8FWCOFdlDV0UxRZAx4rFIth1RZBv2HO1ELuD70LZC9qRf3vtotjZBAZDZD";
-
 function sendTextMessage(sender, text) {
   messageData = {
     text:text
@@ -53,6 +37,24 @@ function sendTextMessage(sender, text) {
     }
   });
 }
+
+app.post('/webhook', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      console.log(text);
+      sendTextMessage(sender, text);
+    }
+  }
+  res.status(200);
+});
+
+var token = "EAAV2q4QDamwBABUMgjZBOyGRt8d8x9qZAO27CFEfbnZAOYZADrMe2B2oHIYQvHPVZCBTZB4sJ98LqF58ZBPmFOrTzp65XYsS0ZAeCw26LukdUCD6Q9r4w8M45HKi8FWCOFdlDV0UxRZAx4rFIth1RZBv2HO1ELuD70LZC9qRf3vtotjZBAZDZD";
+
+
 
 app.listen(process.env.OPENSHIFT_NODEJS_PORT || 80, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", function () {
   console.log('Example app listening on port 80!');
