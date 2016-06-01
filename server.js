65d1f54ab66a32684d8c409fafa720093e87300f
Prithvi Jutur <prithvij@yahoo-inc.com>
2016-06-01 12:13:51 -0700

    fix issues

--- server.js
+++ server.js
@@ -1,106 +1,28 @@
-'use strict'
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// parse application/json
-app.use(bodyParser.json())
-
-// index
 app.get('/', function (req, res) {
-  res.send('I live to serve, my master')
-})
+  res.send('Your wish is my command');
+});
 
-// for facebook verification
-app.get('/webhook/', function (req, res) {
+app.get('/webhook', function (req, res) {
   if (req.query['hub.verify_token'] === 'a_bot_is_a_bot') {
-    res.send(req.query['hub.challenge'])
+    res.send(req.query['hub.challenge']);
+    console.log("validation token successful");
+    res.status(200);
   }
-  res.send('Error, wrong token')
+  res.send('Error, wrong validation token');
+  res.status(401);
 })
-
-// to post data
-app.post('/webhook/', function (req, res) {
-  var messaging_events = req.body.entry[0].messaging
-  for (var i = 0; i < messaging_events.length; i++) {
-    var event = req.body.entry[0].messaging[i]
-    var sender = event.sender.id
-    if (event.message && event.message.text) {
-      var text = event.message.text
-      if (text === 'Generic') {
-        sendGenericMessage(sender)
-        continue
-      }
-      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-    }
-    if (event.postback) {
-      var text = JSON.stringify(event.postback)
-      sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-      continue
-    }
-  }
-  res.status(200)
-})
-
-
-var token = "EAAV2q4QDamwBANjMWinZC46BjVpu91AaK2C7jpttWrYgsupKRc2nAqSpmRsgLqcDxjiRWEu8pFC9DmujczuLbWExoL7xz9nsFNrBhsjbMgAqCgD3uRbVM3GxMNBpYSp3xMePnkQea7F2E6yQF87hIsR1nHThuP3Fj27fFTgZDZD"
 
 function sendTextMessage(sender, text) {
-  var messageData = { text:text }
-  
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending messages: ', error)
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error)
-    }
-  })
-}
-
-function sendGenericMessage(sender) {
-  var messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [{
-          "title": "First card",
-          "subtitle": "Element #1 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://www.messenger.com",
-            "title": "web url"
-          }, {
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for first element in a generic bubble",
-          }],
-        }, {
-          "title": "Second card",
-          "subtitle": "Element #2 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-          "buttons": [{
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for second element in a generic bubble",
-          }],
-        }]
-      }
-    }
+  messageData = {
+    text:text
   }
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
@@ -112,12 +34,30 @@ function sendGenericMessage(sender) {
     }
   }, function(error, response, body) {
     if (error) {
-      console.log('Error sending messages: ', error)
+      console.log('Error sending message: ', error);
     } else if (response.body.error) {
-      console.log('Error: ', response.body.error)
+      console.log('Error: ', response.body.error);
     }
-  })
+  });
 }
+
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      console.log(text);
+      sendTextMessage(sender, text);
+    }
+  }
+  res.status(200);
+});
+
+var token = "EAAV2q4QDamwBANjMWinZC46BjVpu91AaK2C7jpttWrYgsupKRc2nAqSpmRsgLqcDxjiRWEu8pFC9DmujczuLbWExoL7xz9nsFNrBhsjbMgAqCgD3uRbVM3GxMNBpYSp3xMePnkQea7F2E6yQF87hIsR1nHThuP3Fj27fFTgZDZD";
+
+
 
 app.listen(process.env.OPENSHIFT_NODEJS_PORT || 80, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", function () {
   console.log('Example app listening on port 80!');
